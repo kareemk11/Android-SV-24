15f771d8bf357b961d03355981a793d887a6827e
Kareem <kareem.ashraf.rashed@gmail.com>
2024-07-18 09:33:33 +0300

    Adding changes

--- HelloITI.java
+++ HelloITI.java
@@ -1,12 +1,20 @@
-class ITI 
-{
-	void printHelloITI()
-	{
-		x=10;
-	}
-	void printHelloGit()
-	{}
-	
-	void doSomthig()
-	{}
+
+import org.json.simple.JSONObject;
+ 
+// Program for print data in JSON format.
+class JavaJsonEncoding {
+    public static void main(String args[])
+    {
+        // In java JSONObject is used to create JSON object
+        // which is a subclass of java.util.HashMap.
+ 
+        JSONObject file = new JSONObject();
+ 
+        file.put("Full Name", "Ritu Sharma");
+        file.put("Roll No.", new Integer(1704310046));
+        file.put("Tuition Fees", new Double(65400));
+ 
+        // To print in JSON format.
+        System.out.print(file);
+    }
 }
